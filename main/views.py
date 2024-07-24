5236682ec849d957abdf3b597b57fa0576466085
UndeoD <kiajahdas@mail.ru>
2024-07-25 02:36:00 +0300

    The main app is rdy pages index and about

--- main/views.py
+++ main/views.py
@@ -3,14 +3,19 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Page content',
-        'list': ['first', 'second', 'third'],
-        'dict': {'key1': 'value1', 'key2': 'value2'},
-        'bool': True
+        'title': 'Home - Main page',
+        'content': 'Showcase'
+
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse("about")
+    context = {
+        'title': 'About us',
+        'content': 'About us',
+        'text_on_page': "1. you're not a clown. just because you put a gul on your ava, spam zxczczxczcxczcxczcxzxzxc, pause after every kill for no reason and are just a poser - that doesn't mean you're a gul and you have no right to call yourself one.",
+
+    }
+
+    return render(request, 'main/about.html', context)
