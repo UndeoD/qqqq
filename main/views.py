b72e55755a55bb0250e547362a1d04d600f44b1c
UndeoD <kiajahdas@mail.ru>
2024-08-23 23:04:47 +0300

    goods

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Main page',
-        'content': 'Showcase'
-
+        'content': 'Showcase',
+        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
