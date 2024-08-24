193b152f863e0f768756f5ce6e8ec53ce6709688
UndeoD <kiajahdas@mail.ru>
2024-08-24 12:47:06 +0300

    qweqwe

--- goods/models.py
+++ goods/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
         db_table = 'product'
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
+        ordering = ("id",)
 
     def __str__(self):
         return f'{self.name} Quantity: {self.quantity}'
